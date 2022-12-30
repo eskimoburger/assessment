987d38750c19e7f18e0ac61ae83e605a26a1ba59
Eskimoburger <soontornp60@nu.ac.th>
2022-12-30 15:44:03 +0700

    Add echo, Add create_handler route and Port env to start server

--- server.go
+++ server.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 
+	"github.com/eskimoburger/assessment/handler"
+	"github.com/labstack/echo/v4"
 	_ "github.com/lib/pq"
 )
 
@@ -29,5 +31,11 @@ func main() {
 	if err != nil {
 		log.Fatal("can't create table", err)
 	}
+	h := handler.NewApplication(db)
+	e := echo.New()
+
+	e.POST("/expenses", h.CreateExpenseHandler)
+
+	log.Fatal(e.Start(os.Getenv("PORT")))
 
 }
