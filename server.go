9ddd0f33b411d5362e1296e9ff0509aecbe41cb7
Eskimoburger <soontornp60@nu.ac.th>
2022-12-30 18:13:54 +0700

    Add auth middleware

--- server.go
+++ server.go
@@ -4,12 +4,23 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/eskimoburger/assessment/handler"
 	"github.com/labstack/echo/v4"
 	_ "github.com/lib/pq"
 )
+
+func AuthorizationMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		authHeader := c.Request().Header.Get("Authorization")
+		if authHeader != os.Getenv("Authorization") {
+			return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Unauthorized"})
+		}
+		return next(c)
+	}
+}
 
 func main() {
 	fmt.Println("Please use server.go for main file")
@@ -33,7 +44,7 @@ func main() {
 	}
 	h := handler.NewApplication(db)
 	e := echo.New()
-
+	e.Use(AuthorizationMiddleware)
 	e.POST("/expenses", h.CreateExpenseHandler)
 	e.GET("/expenses/:id", h.GetExpenseByIDHandler)
 	e.PUT("/expenses/:id", h.UpdateExpenseHandler)
